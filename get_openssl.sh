b6ac8b336041c913967670d48500d9e5dfd48cd3
Roman Gershman <romange@gmail.com>
2025-07-30 20:22:02 +0300

    chore: fixes

--- get_openssl.sh
+++ get_openssl.sh
@@ -3,9 +3,13 @@ set -e
 
 OPENSSL_VERSION="3.0.17"
 OPENSSL_DIR="/usr/local/ssl"
+CONFIGURE_PARAMS="no-shared no-tests no-idea no-mdc2 no-rc5 no-zlib no-ui-console no-ssl3 no-ssl3-method enable-rfc3779 enable-cms no-capieng no-rdrand"
 
-# Remove installed version
-apt remove -y openssl
+# Add architecture-specific parameters
+if [ "$(uname -m)" = "x86_64" ]; then
+    CONFIGURE_PARAMS="${CONFIGURE_PARAMS} enable-ec_nistp_64_gcc_128"
+fi
+echo "Configuring OpenSSL with parameters: ${CONFIGURE_PARAMS}"
 
 # Download and extract
 cd /tmp
@@ -14,10 +18,7 @@ tar xzf openssl-${OPENSSL_VERSION}.tar.gz
 cd openssl-${OPENSSL_VERSION}
 
 # Configure with static linking
-./Configure linux-x86_64 no-shared --prefix=${OPENSSL_DIR} 
-make -j"$(nproc)"
-make install
-
-ln -s /usr/local/ssl/lib64/libcrypto.a /usr/lib/x86_64-linux-gnu/
-ln -s /usr/local/ssl/lib64/libssl.a /usr/lib/x86_64-linux-gnu/
-cp -r /usr/local/ssl/include/openssl /usr/include/
+./Configure ${CONFIGURE_PARAMS} --prefix=${OPENSSL_DIR} --libdir=lib
+make -j"$(nproc)" build_sw
+make install_sw
+rm -rf /tmp/openssl-*
--- u20.04-dev.Dockerfile
+++ u20.04-dev.Dockerfile
@@ -21,10 +21,14 @@ RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 40  \
     --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 RUN /tmp/get_mold.sh
-RUN /tmp/get_openssl.sh
 RUN /tmp/install_versioned_redis.sh redis-server redis-server-6.2.11 https://github.com/redis/redis/archive/6.2.11.tar.gz
 RUN /tmp/install_versioned_redis.sh redis-server redis-server-7.2.2 https://github.com/redis/redis/archive/7.2.2.tar.gz
 RUN /tmp/install_versioned_redis.sh valkey-server valkey-server-8.0.1 https://github.com/valkey-io/valkey/archive/refs/tags/8.0.1.tar.gz
+
+RUN /tmp/get_openssl.sh
+
+# Set OpenSSL path for CMake
+ENV CMAKE_PREFIX_PATH=/usr/local/ssl  
 
 RUN mkdir -p ~/.config/gdb && echo 'set history save on' >> ~/.config/gdb/gdbinit \
     && echo 'set debuginfod enabled off' >> ~/.config/gdb/gdbinit
